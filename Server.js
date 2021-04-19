cdd629ab239420b215b4a814b236fe4ba86df3fd
Joseph <JSPenland97@gmai.com>
2021-04-19 18:47:55 -0400

    heroku fix

--- Server.js
+++ Server.js
@@ -1,70 +1,21 @@
-//dependencies
-const fs = require("fs");
-const express = require("express");
-const path = require("path");
+// Dependencies
+const express = require('express');
 
-// Sets up the Express App
+// Set up express
 const app = express();
-// Sets port for listening and let heroku decide on port, if not, use port 8080
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
-//serve images, CSS files, and JavaScript files in a directory named public
-app.use(express.static('public'));
+// /public line is needed in order to access CSS files properly
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-//route to notes.html
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
 
-//route to read the `db.json` file and return all saved notes as JSON.
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
 
-//route to index.html aka main page
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-//receive a new note to save on the request body, add it to the `db.json` file, 
-//and then return the new note to the client.
-app.post("/api/notes", (req, res) => {
-    let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let notelength = (noteList.length).toString();
-
-    //create new property called id based on length and assign it to each json object
-    newNote.id = notelength;
-    //push updated note to the data containing notes history in db.json
-    noteList.push(newNote);
-
-    //write the updated data to db.json
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-    res.json(noteList);
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
 })
-
-//delete note according to their tagged id.
-app.delete("/api/notes/:id", (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteId = (req.params.id).toString();
-
-    //filter all notes that does not have matching id and saved them as a new array
-    //the matching array will be deleted
-    noteList = noteList.filter(selected =>{
-        return selected.id != noteId;
-    })
-
-    //write the updated data to db.json and display the updated note
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-    res.json(noteList);
-});
-
-
-//listen tot he port when deployed
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
---
+++ routes/apiRoutes.js
@@ -0,0 +1,95 @@
+// Require db file and the fs module
+const dbJSON = require('../db/db.json');
+const fs = require('fs');
+
+// NPM unique ID module
+module.uniqid_debug = true;
+var uniqid = require('uniqid');
+
+
+module.exports = (app) => {
+    //Get the entire db.json object in JSON format
+    app.get('/api/notes', (req, res) => {
+        // Use FS to read the latest version of db.json
+        fs.readFile('db/db.json', 'utf8', (err, data) => {
+            if (err) { console.err(err) }
+            else {
+                res.json(JSON.parse(data));
+                // console.log(data);
+            }
+        })
+    })
+
+    // Get individual items from db.json, based on the ID's; response given in JSON format
+    app.get('/api/notes/:id', (req, res) => {
+        // NoteId grabs the id from the url
+        const noteId = req.params.id;
+        // Use FS to read the latest version of db.json
+        fs.readFile('db/db.json', 'utf8', (err, data) => {
+            if (err) { console.err(err) }
+            else {
+                // use a for loop to find the specific item in the db.json array
+                for (let i = 0; i < dbJSON.length; i++) {
+                    if (noteId === dbJSON[i].id) {
+                        return res.json(dbJSON[i]);
+                    }
+                }
+            }
+        })
+    })
+
+   
+    // Post - Create a new Note after clicking the Save Button
+    app.post('/api/notes', (req, res) => {
+        fs.readFile('db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.err(err);
+            } else {
+                // Parse data from db.JSON so we can add to it
+                const notesDb = JSON.parse(data);
+                // Variable for the entered new note data
+                const newNote = req.body;
+                // variables to create a unique ID
+                const idKey = "id";
+                const noteId = uniqid();
+                // Add ID to newNote
+                newNote[idKey] = noteId;
+                // push completed note to notesDb variable
+                notesDb.push(newNote);
+                //Use fs Write file to rewrite / replace the entire contents of db.json
+                fs.writeFile('db/db.json', JSON.stringify(notesDb), (err) => {
+                    if (err) throw err;
+                    res.json(notesDb);
+                });
+            };
+        });
+    });
+
+    // Deleting Notes
+    app.delete('/api/notes/:id', (req, res) => {
+        // NoteId grabs the id from the url
+        const noteId = req.params.id;
+        // Use FS to read the latest version of db.json
+        fs.readFile('db/db.json', 'utf8', (err, data) => {
+            const notesDb = JSON.parse(data);
+            if (err) { console.err(err) }
+            else {
+                // use a for loop to find the specific item in the db.json array
+                for (let i = 0; i < notesDb.length; i++) {
+                    // If array item # i's ID matches req.params.id
+                    if (noteId === notesDb[i].id) {
+                        // Use Splice to remove the item from the array
+                        notesDb.splice([i], 1)
+                        // write back into the db.json file
+                        fs.writeFile('db/db.json', JSON.stringify(notesDb), (err) => {
+                            if (err) throw err;
+                            // respond with the latest updated array
+                            res.json(notesDb);
+                        });
+                    }
+                }
+            }
+        });
+    });
+
+}
